4161bbf5c3f18519846fb488113f6a1748776ebe
AshishTiwari <ashish.t@inspironlabs.com>
2026-01-06 22:41:45 +0530

    Add Spring Boot backend source code and Maven setup

---
+++ src/main/java/com/AiAgent/InnerSparkPathWay/InnerSparkPathWayApplication.java
@@ -0,0 +1,13 @@
+package com.AiAgent.InnerSparkPathWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerSparkPathWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerSparkPathWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AiAgent/InnerSparkPathWay/InnerSparkPathWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AiAgent.InnerSparkPathWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerSparkPathWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
